9ff2f4e216546c64e2a5562d279a2bb87483d387
kevincabrera14 <ka5849698@gmail.com>
2026-04-19 14:34:40 -0500

    maps

--- properties/forms.py
+++ properties/forms.py
@@ -40,17 +40,6 @@ def parse_google_maps_url(url):
 class PropertyForm(forms.ModelForm):
     """Formulario para crear/editar propiedades."""
 
-    # Campo auxiliar para la URL de Google Maps
-    maps_url = forms.CharField(
-        label='Ubicación en Google Maps',
-        required=False,
-        help_text='Pega aquí la URL de Google Maps o las coordenadas (lat, lng) para ubicar la propiedad en el mapa.',
-        widget=forms.TextInput(attrs={
-            'class': 'form-control',
-            'placeholder': 'https://www.google.com/maps/@6.2442,-75.5812,15z o -6.2442, -75.5812'
-        })
-    )
-
     class Meta:
         model = Property
         fields = [
@@ -74,34 +63,6 @@ class PropertyForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['municipality'].queryset = Municipality.objects.select_related('zone').order_by('zone__order', 'order')
-
-        # Si estamos editando, pre-llenar el campo maps_url con las coordenadas
-        if self.instance and self.instance.pk:
-            if self.instance.latitude and self.instance.longitude:
-                self.initial['maps_url'] = f"{self.instance.latitude},{self.instance.longitude}"
-
-    def clean_maps_url(self):
-        url = self.cleaned_data.get('maps_url', '')
-        if url:
-            lat, lng = parse_google_maps_url(url)
-            if lat is None:
-                raise forms.ValidationError('No se pudieron extraer coordenadas de la URL. Asegúrate de que sea una URL válida de Google Maps o coordenadas (lat, lng).')
-            self.cleaned_data['latitude'] = lat
-            self.cleaned_data['longitude'] = lng
-        return url
-
-    def save(self, commit=True):
-        instance = super().save(commit=False)
-
-        # Guardar coordenadas si fueron proporcionadas
-        if 'latitude' in self.cleaned_data:
-            instance.latitude = self.cleaned_data['latitude']
-        if 'longitude' in self.cleaned_data:
-            instance.longitude = self.cleaned_data['longitude']
-
-        if commit:
-            instance.save()
-        return instance
 
 
 class PropertyImageForm(forms.ModelForm):
