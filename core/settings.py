ae0318c510a0ddf744d0b22d6609da0e477067a9
kevincabrera14 <ka5849698@gmail.com>
2026-04-26 19:15:42 -0500

    imagenes

--- core/settings.py
+++ core/settings.py
@@ -100,7 +100,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # ARCHIVOS MEDIA (imágenes de propiedades)
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.environ.get('MEDIA_ROOT', str(BASE_DIR / 'media'))
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.User'
